cc7674a938b334ea828041021d9d4c0e613cb914
Matvey Kolesnikov <mkolesnikov@alabuga.ru>
2026-06-23 01:33:11 +0300

    Force dark Zen browser chrome

--- .config/zen/user.js
+++ .config/zen/user.js
@@ -2,5 +2,8 @@
 // Keep userChrome.css enabled without editing prefs.js while Zen is running.
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.uidensity", 1);
-user_pref("browser.theme.toolbar-theme", 0);
-user_pref("browser.theme.content-theme", 0);
+user_pref("browser.theme.toolbar-theme", 1);
+user_pref("browser.theme.content-theme", 1);
+user_pref("layout.css.prefers-color-scheme.content-override", 0);
+user_pref("ui.systemUsesDarkTheme", 1);
+user_pref("browser.display.background_color", "#060d14");
